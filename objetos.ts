0d0e8c12754f2a715498c0ab1a1f558fa2a45a90
Federico <fedee.unates.2001@gmail.com>
2023-12-18 16:47:44 -0300

    union types

--- objetos.ts
+++ objetos.ts
@@ -22,11 +22,82 @@
 
 // ------- OPTIONALS PROPERTIES ---------
 
+// type HeroId = `${string}-${string}-${string}-${string}-${string}`
+
+// type Hero = {
+//   readonly id?: HeroId;
+//   name: string;
+//   age: number;
+//   isActive?: boolean;
+// };
+
+// let hero: Hero = {
+//   name: "Thor",
+//   age: 1500,
+// };
+
+// /**
+//  * Devuelve un Hero
+//  */
+// function createHero(hero: Hero): Hero {
+//   const { name, age } = hero;
+//   const id = crypto.randomUUID();
+//   return {id, name, age, isActive: true };
+// }
+
+// const thor = createHero({ name: "Thor", age: 1500 });
+// //Lo autocompleta
+
+// console.log(crypto.randomUUID());
+
+//template union types
+// type HexadecimalColor =`#${string}`
+
+// const color: HexadecimalColor = '0033ff'
+// const color2: HexadecimalColor = "#0033ff"
+
+//---------- UNION TYPES ----------
+
+// type HeroId = `${string}-${string}-${string}-${string}-${string}`;
+// type heroPowerScale = "low" | "medium" | "high";
+
+// type Hero = {
+//   readonly id?: HeroId;
+//   name: string;
+//   age: number;
+//   isActive?: boolean;
+//   powerScale?: heroPowerScale
+// };
+
+// let hero: Hero = {
+//   name: "Thor",
+//   age: 1500,
+// };
+
+// /**
+//  * Devuelve un Hero
+//  */
+// function createHero(hero: Hero): Hero {
+//   const { name, age } = hero;
+//   const id = crypto.randomUUID();
+//   return { id, name, age, isActive: true };
+// }
+
+// const thor = createHero({ name: "Thor", age: 1500 });
+
+// thor.powerScale= "high"
+
+// ------ INTERSECTION TYPES -----
+
+type HeroId = `${string}-${string}-${string}-${string}-${string}`;
+type heroPowerScale = "low" | "medium" | "high";
+
 type Hero = {
-  readonly id?: string;
+  readonly id?: HeroId;
   name: string;
   age: number;
   isActive?: boolean;
+  powerScale?: heroPowerScale;
 };
 
 let hero: Hero = {
@@ -39,11 +110,10 @@ let hero: Hero = {
  */
 function createHero(hero: Hero): Hero {
   const { name, age } = hero;
-  // const id = crypto.randomUUID();
-  return { name, age, isActive: true };
+  const id = crypto.randomUUID();
+  return { id, name, age, isActive: true };
 }
 
 const thor = createHero({ name: "Thor", age: 1500 });
-//Lo autocompleta
 
-console.log(crypto.randomUUID());
+thor.powerScale = "high";
